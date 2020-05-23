1f383b1f07d90375a6682b400bdd4fbb2fa7be0a
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-23 22:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function wrapGifts(name, event) { 
   for (let i = 0; names.length; i++) {
-    names[i] = 
+    names[i] = ('Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
+  }
+  return names;
   }
