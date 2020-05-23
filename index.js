2464a8308cfcac5a2000b0c4e67beaa542f9d466
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-23 22:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-writeCards = (["Ada", "Brendan", "Ali"], "birthday");
-
 function wrapGifts(name, event) { 
   for (let i = 0; names.length; i++) {
     names[i] = (`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
